60089f015cbd5a8e2001961c55b577883eb65be3
SubhaeSadeek <nurun.nabi@outlook.com>
2025-11-18 23:07:24 +0600

    finalised problems and the blog

--- solution.ts
+++ solution.ts
@@ -118,32 +118,3 @@ const calculateTotalPrice = (
 
 	return totalPriceOfAllProduct;
 };
-
-// __________-------------------------------------
-// ?__________-------------------------------------
-// !__________-------------------------------------
-
-const getSwallowVelocity = (type: "african" | "european"): string => {
-	if (type === "african") {
-		return "Roughly 11 meters per second.";
-	}
-	return "I... I don't know that!";
-};
-
-const isCatLiquid = (isAsleep: boolean, containerShape: string): boolean => {
-	return isAsleep && containerShape !== "none";
-};
-
-class Wizard {
-	name: string;
-	favoriteSpell: string;
-
-	constructor(name: string, favoriteSpell: string) {
-		this.name = name;
-		this.favoriteSpell = favoriteSpell;
-	}
-
-	castSpell(): string {
-		return `${this.name} casts ${this.favoriteSpell}!`;
-	}
-}
